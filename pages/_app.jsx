7317952658ac21d9107f9602b49012c15fd91cd7
ehsansh9574-hue <ehsan.sh9574@gmail.com>
2025-11-07 19:37:41 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,3 @@
-import "../styles/globals.css";
-import Layout from "../components/Layout";
-
-export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
