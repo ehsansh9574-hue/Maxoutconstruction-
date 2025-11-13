e6fa3391a282c58d639cf458c16083ed828d3397
ehsansh9574-hue <ehsan.sh9574@gmail.com>
2025-11-13 09:57:51 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,11 @@
+// pages/_app.jsx
+import "../styles/globals.css"; // ok if this file doesn't exist
+import Layout from "../components/Layout";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
